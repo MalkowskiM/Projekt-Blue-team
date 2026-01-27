e38df5d0c53120b632f83788fc49aff5458b0cae
MateuszM <mmalkowski315@gmail.com>
2026-01-27 17:53:21 +0100

    Added unit tests

---
+++ src/test/java/com/example/thymeleaf/controller/ExceptionHandlerControllerTest.java
@@ -0,0 +1,45 @@
+package com.example.thymeleaf.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ExceptionHandlerControllerTest {
+
+    @InjectMocks
+    private ExceptionHandlerController sut;
+
+    @Test
+    void shouldReturnNotFoundException() {
+        //when
+        ModelAndView modelAndView = sut.noSuchElementExceptionHandler();
+
+        //then
+        assertThat(modelAndView.getViewName()).isEqualTo("exception");
+        assertThat(modelAndView.getModel()).isEqualTo(Map.of("status", HttpStatus.NOT_FOUND.value()));
+
+    }
+
+
+    @Test
+    void shouldReturnDefaultException() {
+        //when
+        ModelAndView modelAndView = sut.defaultExceptionHandler();
+
+        //then
+        assertThat(modelAndView.getViewName()).isEqualTo("exception");
+        assertThat(modelAndView.getModel()).isEqualTo(Map.of("status", HttpStatus.INTERNAL_SERVER_ERROR.value()));
+
+    }
+
+
+}
